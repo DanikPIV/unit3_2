0ac2320ac8b7d9658b956c28703706b9741d810c
Daniil Posvenchuk <posvenchuk.danik@gmail.com>
2023-04-20 23:29:47 +0300

    release

--- unit3_2/Program.cs
+++ unit3_2/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace unit3_2
 {
+
     internal class Program
     {
+        public static string TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    int sum = nums[i] + nums[j];
+                    if (sum == target)
+                    {
+                        string indexes = $"[{i}, {j}]";
+                        return indexes;
+                    }
+                }
+            }
+            return "Такое число нельзя получить суммой двух элементов из массива";
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите чисело");
+            int target = int.Parse(Console.ReadLine());
+            int[] nums = { 2, 7, 11, 15 };
+            Console.WriteLine(TwoSum(nums, target));
+            Console.ReadKey();
         }
     }
 }
